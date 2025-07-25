0b77bb93bf94e3257ded014a5473f575cab7e477
Daniel Naiff <danielnaiff344@gmail.com>
2025-07-25 20:20:38 -0300

    feat(validation): add validation in request fields

--- loans/loans/src/main/java/com/danielnaiff/loans/Entities/dtos/CustomerResquestDTO.java
+++ loans/loans/src/main/java/com/danielnaiff/loans/Entities/dtos/CustomerResquestDTO.java
@@ -1,6 +1,26 @@
 package com.danielnaiff.loans.Entities.dtos;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 
-public record CustomerResquestDTO(int age, String cpf, String name, Double income, String location) {
+public record CustomerResquestDTO(
+        @NotBlank(message = "Age is mandatory")
+        int age,
+
+        @NotBlank(message = "CPF is mandatory")
+        String cpf,
+
+        @NotBlank(message = "Name is mandatory")
+        String name,
+
+        @NotNull(message = "Income is mandatory")
+        @DecimalMin(value = "0.0", inclusive = false, message = "Income must be positive")
+        Double income,
+
+        @NotBlank(message = "Location is mandatory")
+        String location
+) {
 }
